0d03e280ba5b6280ff65d9be27ba3ff543362b5e
yufusuzi <1515792195@qq.com>
2018-12-31 17:19:28 +0800

    test

---
+++ src/main/java/cn/nemofish/test/HelloWord.java
@@ -0,0 +1,16 @@
+/**
+ * @author 余夫俗子
+ * @date 2018年12月31日下午5:13:45
+ * 
+ */
+package cn.nemofish.test;
+
+/**
+ * @author 余夫俗子
+ * @date 2018年12月31日下午5:13:45
+ */
+public class HelloWord {
+	public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
