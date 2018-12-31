ff5522b518c792936264b08c0433c3e7ff9e0121
yufusuzi <1515792195@qq.com>
2018-12-31 17:57:03 +0800

    sf

--- src/main/java/cn/nemofish/test/HelloWord.java
+++ src/main/java/cn/nemofish/test/HelloWord.java
@@ -11,6 +11,6 @@ package cn.nemofish.test;
  */
 public class HelloWord {
 	public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Worsfasdfasld");
     }
 }
